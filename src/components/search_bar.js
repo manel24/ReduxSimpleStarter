5c072a3b210d5a7ec6efadce2de5a4f0fa02d3c9
ManelCH <manelcharouni@think-it.io>
2018-06-27 11:05:23 +0100

    testing the youtubeSearch-API

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "initial input" };
+  }
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+        input: {this.state.term}
+      </div>
+    );
+  }
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTsearch from "youtube-api-search";
 
+import SearchBar from "./components/search_bar";
+const API_KEY = "AIzaSyDh7C7SbrEgfPaBv6kKEmvCCq4eckQRtGY";
+
+YTsearch({ key: API_KEY, term: "surfboard" }, function(data) {
+  console.log(data);
+});
 //Create a new component.
 //this component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 //Take this component's generated HTML and put it in the DOM
