849317034be69b0b9a9a08fc12727ab80aad2515
ManelCH <manelcharouni@think-it.io>
2018-06-28 11:16:11 +0100

    adding the VideoList component

---
+++ src/components/video_list_item.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const VideoListItem = ({
+  video
+}) /*it will fetch the video prop automatically*/ => {
+  const imgUrl = video.snippet.thumbnails.default.url;
+  const video_heading = video.snippet.title;
+  return (
+    <li className="list-group-item">
+      <div className="video-list-media">
+        <div className="media-left">
+          <img className="media-object" src={imgUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video_heading}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTsearch from "youtube-api-search";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+
 
 import SearchBar from "./components/search_bar";
 const API_KEY = "AIzaSyDh7C7SbrEgfPaBv6kKEmvCCq4eckQRtGY";
 
-YTsearch({ key: API_KEY, term: "surfboard" }, function(data) {
-  console.log(data);
-});
-//Create a new component.
-//this component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTsearch(
+      {
+        key: API_KEY,
+        term: "surfboard"
+      },
+      videos => this.setState({ videos })
+    );
+  }
 
-//Take this component's generated HTML and put it in the DOM
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
